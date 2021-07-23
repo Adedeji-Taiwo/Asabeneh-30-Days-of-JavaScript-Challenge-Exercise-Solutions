88f84e96212921e197b0074b25fea6cac3cb354b
Dhegyaino <dtaiwo45@gmail.com>
2021-07-23 16:55:29 +0100

    Add files via upload

---
+++ DAY 1 INTRODUCTION/datatypes.js
@@ -0,0 +1,14 @@
+//Create datatypes.js file and use the JavaScript typeof operator to check different data types. Check the data type of each variable
+let firstName = "Femi";
+let age = 20;
+let work = false;
+let shoe = null;
+let sac;
+
+
+
+console.log(typeof firstName);
+console.log(typeof age); 
+console.log(typeof work);
+console.log(typeof shoe);
+console.log(typeof sac);
---
+++ DAY 1 INTRODUCTION/variable.js
@@ -0,0 +1,76 @@
+/*Write a single line comment which says, comments can make code readable*/
+//Comments can make codes readable
+
+/*Write another single comment which says, Welcome to 30DaysOfJavaScript*/
+//Welcome to 30 days of Javascript Challenge
+
+//Write a multiline comment which says, comments can make code readable, easy to reuse and informative
+/*Comments can make codes codes readable, 
+easy to understand, easy to reuse and informative*/
+
+
+//Create a variable.js file and declare variables and assign string, boolean, undefined and null data types
+let lastName = "Adedeji";
+let butt = true;
+let height
+let rag = null;
+
+console.log(lastName);
+console.log(butt);
+console.log(height);
+console.log(rag);
+
+//Declare four variables without assigning values
+let condo
+let hill
+let me
+let them
+
+console.log(condo);
+console.log(hill);
+console.log(me);
+console.log(them);
+
+//Declare four variables with assigned values
+const PI = 3.142;
+const gravity = 9.81;
+let state = "Lagos";
+let school = "funaab";
+
+
+
+console.log(PI);
+console.log(gravity);
+console.log(state);
+console.log(school);
+
+
+//Declare variables to store your first name, last name, marital status, country and age in multiple lines
+let fName = "Adedeji";
+let lName = "Taiwo";
+let mStatus = "Single";
+let country = "Nigeria";
+let ages = 100;
+
+console.log(fName);
+console.log(lName);
+console.log(mStatus);
+console.log(country);
+console.log(ages);
+console.log(fName, lName, mStatus, country, ages);
+
+
+//Declare variables to store your first name, last name, marital status, country and age in a single line
+let myFirstName = "Micky"; myLastName = "Tee"; maritalStatus = "single";
+
+//Declare two variables myAge and yourAge and assign them initial values and log to the browser console.
+let myAge = "I am 25 years old.";
+let yourAge = "You are 30 years old.";
+
+console.log(myAge);
+console.log(yourAge);
+
+
+
+
+
