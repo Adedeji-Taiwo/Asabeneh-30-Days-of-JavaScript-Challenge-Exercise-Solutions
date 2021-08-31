26373adab78ce43c8950fb9006dc16c95396f554
Dhegyaino <dtaiwo45@gmail.com>
2021-08-31 13:28:55 +0100

    Add files via upload

---
+++ DAY 5 ARRAYS/main.js
@@ -0,0 +1,7 @@
+//Create a separate countries.js file and store the countries array in to this file, create a separate file web_techs.js and store the webTechs array in to this file. Access both file in main.js file
+import {countries} from "./countries.js";
+import {webTech} from "./webTech.js";
+
+
+console.log(countries);
+console.log(webTech);
---
+++ DAY 5 ARRAYS/webTech.js
@@ -0,0 +1,12 @@
+const webTech = [
+    'HTML',
+    'CSS',
+    'JavaScript',
+    'React',
+    'Redux',
+    'Node',
+    'MongoDB',
+    'PHP',
+];
+
+export {webTech};
